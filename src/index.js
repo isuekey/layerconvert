c868c5ca65202b925c542f1ec6a5a8f0e17d6dbe
liuhanru <isuekey@163.com>
2020-08-11 17:26:48 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
-import * as rule from './rule';
-import * as down from './down';
-import * as upstream from './upstream';
+import * as rule from './rule.js';
+import * as down from './down.js';
+import * as upstream from './upstream.js';
 
 export {
   rule,
