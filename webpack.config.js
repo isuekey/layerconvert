5db048474fd1e22205b17a7d233831a38081f7b9
liuhanru <isuekey@163.com>
2021-03-09 13:26:48 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
       { test: /\.tsx?$/, loader: "ts-loader" },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" },
     ],
   },
 };
